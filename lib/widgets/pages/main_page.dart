6ba64cddc667c651938087cfc248b7ba855061ff
Haesun Lee <hlee830@gatech.edu>
2022-11-14 16:00:56 -0500

    updated with navigator

--- lib/widgets/pages/main_page.dart
+++ lib/widgets/pages/main_page.dart
@@ -2,62 +2,37 @@
 import 'package:flutter/material.dart';
 import 'package:tap_review/routes/routes.dart';
 
-void main() {
-  runApp(const MyApp());
-}
-
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  // This widget is the root of your application.
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'TapReview',
-      theme: ThemeData(
-        primarySwatch: Colors.indigo,
-      ),
-      home: const MyHomePage(title: 'TapReview'),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  final String title;
+class MainPage extends StatefulWidget {
+  const MainPage({super.key});
 
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  State<MainPage> createState() => _MainPageState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-
+class _MainPageState extends State<MainPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: const Text('TapReview'),
-        centerTitle: true,
-        titleTextStyle: const TextStyle(fontSize: 30, color: Colors.white),
-        toolbarHeight: 80,
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-          children: <Widget>[
-            ElevatedButton(
-              onPressed: () {},
-              style: ElevatedButton.styleFrom(
-                fixedSize: const Size(250,150),
-                shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(10),),
-                backgroundColor: Colors.indigo,
-                foregroundColor: Colors.white,
-                textStyle: const TextStyle(fontSize: 40)
+        appBar: AppBar(
+          title: Text('Main Page'),
+        ),
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              ElevatedButton(
+                child: Text('Review'),
+                onPressed: () {
+                  Navigator.of(context).pushNamed(RouteManager.reviewPage);
+                },
+                style: ElevatedButton.styleFrom(
+                    fixedSize: const Size(150, 50),
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(10),
+                    ),
+                    backgroundColor: Colors.indigo,
+                    foregroundColor: Colors.white,
+                    textStyle: const TextStyle(fontSize: 25)),
               ),
               SizedBox(
                 height: 20,
