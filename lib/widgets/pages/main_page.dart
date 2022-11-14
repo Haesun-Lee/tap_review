4140e8a58ebb7f7ee462975456049947a353981f
Jueun Lee <78533448+jlee3711@users.noreply.github.com>
2022-11-14 15:33:35 -0500

    Update main_page.dart

--- lib/widgets/pages/main_page.dart
+++ lib/widgets/pages/main_page.dart
@@ -37,7 +37,7 @@ class _MyHomePageState extends State<MyHomePage> {
       appBar: AppBar(
         // Here we take the value from the MyHomePage object that was created by
         // the App.build method, and use it to set our appbar title.
-        title: const Text('TapReview'),
+        title: const Text('Main Page'),
         centerTitle: true,
         titleTextStyle: const TextStyle(fontSize: 30, color: Colors.white),
         toolbarHeight: 80,
