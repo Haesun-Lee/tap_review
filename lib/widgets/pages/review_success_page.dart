9859d9d6c5382c5810e023d129888455b28dc00b
Jueun Lee <78533448+jlee3711@users.noreply.github.com>
2022-11-15 18:49:15 -0500

    Update review_success_page.dart

--- lib/widgets/pages/review_success_page.dart
+++ lib/widgets/pages/review_success_page.dart
@@ -12,7 +12,27 @@ class _ReviewSuccessPageState extends State<ReviewSuccessPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text('Review Success Page'), backgroundColor: Color(0xFFd52b1c),),
+      backgroundColor: Colors.white,
+      appBar: AppBar(
+        title: Text('Review Success Page'), 
+        backgroundColor: Color(0xFFd52b1c),
+      ),
+      body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget> [
+              Image.asset('assets/thumbs-up.jpeg'),
+              SizedBox(
+                height: 20,
+              ),
+              Text('Thanks for', style: TextStyle(color: Colors.black, fontSize:30, fontWeight:FontWeight.bold, letterSpacing: 1.0)),
+              SizedBox(
+                height: 10,
+              ),
+              Text('submitting a review', style: TextStyle(color: Colors.black, fontSize:30, fontWeight:FontWeight.bold, letterSpacing: 1.0))
+            ]
+          )
+        )
     );
   }
 }
