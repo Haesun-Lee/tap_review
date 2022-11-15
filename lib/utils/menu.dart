ae5f7b3346a9bcf0722bd50ea257f0e4a9d3acd6
Haesun Lee <hlee830@gatech.edu>
2022-11-15 15:58:56 -0500

    updated menu data

--- lib/utils/menu.dart
+++ lib/utils/menu.dart
@@ -17,7 +17,7 @@ class Menu {
 
 List<Menu> MenuList = [
   Menu(
-      menu_name: 'Fries',
+      menu_name: 'Squid Fries',
       description: 'This is very delicious fries made by Haesun',
       imageUrl: 'assets/Fries.jpeg',
       rating: 3),
@@ -29,6 +29,36 @@ List<Menu> MenuList = [
   Menu(
       menu_name: 'Salad',
       description: 'This is a Salad that I had back in Korea',
+      imageUrl: 'assets/Salad.jpeg',
+      rating: 4),
+  Menu(
+      menu_name: 'Steamed Beef',
+      description: 'This is very delicious fries made by Daeun',
+      imageUrl: 'assets/Steamed.jpeg',
+      rating: 4),
+  Menu(
+      menu_name: 'Oil Pasta',
+      description: 'This is a Homemade pasta made by Kahye',
+      imageUrl: 'assets/Oil_Pasta.jpeg',
+      rating: 3),
+  Menu(
+      menu_name: 'Sweet Potato Fries',
+      description: 'This is a Sweet potato fries by Dink Tea',
+      imageUrl: 'assets/SweetPotato.jpeg',
+      rating: 1),
+  Menu(
+      menu_name: 'Lamb Skewer',
+      description: 'This is very delicious lab skewers by Kochi Maru',
+      imageUrl: 'assets/Lamb.jpeg',
+      rating: 5),
+  Menu(
+      menu_name: 'Tteokbokki',
+      description: 'This is a K-Tteokbokki that I had in Korea',
+      imageUrl: 'assets/Tteokbokki.jpeg',
+      rating: 5),
+  Menu(
+      menu_name: 'Bacon Wrapped Pinapple',
+      description: 'This is pinapple wrapped with bacon.',
       imageUrl: 'assets/Salad.jpeg',
       rating: 4),
 ];
