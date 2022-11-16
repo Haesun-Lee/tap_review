c5ebede7acb252fe9c54830b47f34ba36d2f5301
Jueun Lee <78533448+jlee3711@users.noreply.github.com>
2022-11-15 19:32:08 -0500

    Update order_success_page.dart

--- lib/widgets/pages/order_success_page.dart
+++ lib/widgets/pages/order_success_page.dart
@@ -12,7 +12,43 @@ class _OrderSuccessPageState extends State<OrderSuccessPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.white,
       appBar: AppBar(title: Text('Order Success Page'), backgroundColor: Color(0xFFd52b1c),),
+      body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget> [
+              Image.asset('assets/check.jpeg', height:150, width: 150, color: Colors.white.withOpacity(0.9), colorBlendMode: BlendMode.modulate,),
+              SizedBox(
+                height: 20,
+              ),
+              Text(
+                'Order submitted', 
+                style: TextStyle(
+                  color: Colors.black, 
+                  fontSize:40, 
+                  fontWeight:FontWeight.bold, 
+                  letterSpacing: 1.0
+                )
+              ),
+              SizedBox(
+                height: 20,
+              ),
+              Text(
+                'Thanks for your purchase', 
+                style: TextStyle(
+                  color: Colors.black, 
+                  fontSize:25, 
+                  fontWeight:FontWeight.bold, 
+                  letterSpacing: 1.0
+                )
+              ),
+              SizedBox(
+                height: 5,
+              ),
+            ]
+          )
+        )
     );
   }
 }
