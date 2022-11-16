da1e41b10871a45ad6dae82b7e39f8df034420e8
jlee3711 <jlee3711@gatech.edu>
2022-11-15 21:25:35 -0500

    add cart button and add to cart button

--- lib/widgets/pages/menu_detail.dart
+++ lib/widgets/pages/menu_detail.dart
@@ -6,11 +6,25 @@ class MenuDetailPage extends StatelessWidget {
   //const MenuDetailPage({super.key});
   final Menu menu;
   MenuDetailPage(this.menu);
+  
+  int _counter = 0;
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(title: Text(menu.menu_name), backgroundColor: Color(0xFFd52b1c),),
+        appBar: AppBar(
+          title: Text(menu.menu_name), 
+          backgroundColor: Color(0xFFd52b1c),
+          actions: <Widget>[
+            IconButton(
+                      icon: const Icon(Icons.shopping_cart),
+                      iconSize: 30,
+                      onPressed: (){
+                      
+                      },
+            ),
+          ]
+        ),
         body: Padding(
             padding: const EdgeInsets.all(18.0),
             child: Column(
@@ -36,8 +50,37 @@ class MenuDetailPage extends StatelessWidget {
                     textAlign: TextAlign.justify,
                     style: TextStyle(fontSize: 15.0),
                   ),
-                )
+                ),
+                SizedBox(
+                  height: 170,
+                ),
+                Row(
+                  children: <Widget> [
+                    SizedBox(
+                      width: 20,
+                    ),
+                    ElevatedButton(
+                      child: Text(
+                        'Add to cart',
+                        style: TextStyle(fontSize: 16.0),
+                      ),
+                      onPressed: () {
+
+                      },
+                      style: ElevatedButton.styleFrom(
+                      fixedSize: const Size(130, 50),
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(20),
+                      ),
+                      backgroundColor: Color(0xD9d52b1c),
+                      foregroundColor: Colors.white,
+                      textStyle: const TextStyle(fontSize: 25)),
+                    ),
+                  ]
+                ),
               ],
-            )));
+            )
+          )
+      );
   }
 }
