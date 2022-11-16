83429daa96e5829dede1fca8a9744e9206e72b2d
Haesun Lee <hlee830@gatech.edu>
2022-11-16 11:32:53 -0500

    updated rating bar

--- lib/widgets/pages/menu_detail.dart
+++ lib/widgets/pages/menu_detail.dart
@@ -1,5 +1,6 @@
 // ignore_for_file: prefer_const_constructors
 import 'package:flutter/material.dart';
+import 'package:flutter_rating_bar/flutter_rating_bar.dart';
 import 'package:tap_review/utils/menu.dart';
 
 import '../../routes/routes.dart';
@@ -36,20 +37,26 @@ class MenuDetailPage extends StatelessWidget {
                   fit: BoxFit.fill,
                 ),
                 Padding(
-                  padding: const EdgeInsets.all(8.0),
-                  child: Text(
-                    'Rating : ' + menu.rating.toString(),
-                    textAlign: TextAlign.center,
-                    style:
-                        TextStyle(fontSize: 17.0, fontStyle: FontStyle.italic),
-                  ),
-                ),
+                    padding: const EdgeInsets.all(8.0),
+                    child: RatingBarIndicator(
+                      rating: menu.rating,
+                      itemBuilder: (context, index) => Icon(
+                        Icons.star,
+                        color: Colors.amber,
+                      ),
+                      itemCount: 5,
+                      itemSize: 25.0,
+                      direction: Axis.horizontal,
+                    )),
                 Padding(
                   padding: const EdgeInsets.all(8.0),
                   child: Text(
                     'Description : \n' + menu.description,
                     textAlign: TextAlign.justify,
-                    style: TextStyle(fontSize: 15.0),
+                    style: TextStyle(
+                        fontSize: 15.0,
+                        fontWeight: FontWeight.bold,
+                        color: Color.fromARGB(255, 62, 62, 62)),
                   ),
                 ),
                 SizedBox(
@@ -59,22 +66,25 @@ class MenuDetailPage extends StatelessWidget {
                   SizedBox(
                     width: 20,
                   ),
-                  ElevatedButton(
-                    child: Text(
-                      'Add to cart',
-                      style: TextStyle(fontSize: 16.0),
+                  Padding(
+                    padding: const EdgeInsets.all(20.0),
+                    child: ElevatedButton(
+                      child: Text(
+                        'Add to cart',
+                        style: TextStyle(fontSize: 16.0),
+                      ),
+                      onPressed: () {
+                        Navigator.of(context).pushNamed(RouteManager.menuPage);
+                      },
+                      style: ElevatedButton.styleFrom(
+                          fixedSize: const Size(130, 50),
+                          shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(20),
+                          ),
+                          backgroundColor: Color(0xD9d52b1c),
+                          foregroundColor: Colors.white,
+                          textStyle: const TextStyle(fontSize: 25)),
                     ),
-                    onPressed: () {
-                      Navigator.of(context).pushNamed(RouteManager.menuPage);
-                    },
-                    style: ElevatedButton.styleFrom(
-                        fixedSize: const Size(130, 50),
-                        shape: RoundedRectangleBorder(
-                          borderRadius: BorderRadius.circular(20),
-                        ),
-                        backgroundColor: Color(0xD9d52b1c),
-                        foregroundColor: Colors.white,
-                        textStyle: const TextStyle(fontSize: 25)),
                   ),
                 ]),
               ],
